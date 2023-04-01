8551d07e68262d82ea19441eea2e6f79e8ad9111
Mr.Eldar <elka.bekir@gmail.com>
2023-04-01 19:43:12 -0400

    day12

---
+++ learningday12/looping.py
@@ -0,0 +1,19 @@
+months = ('January', "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December")
+
+for month in months:
+    print(month)
+
+i = len(months) - 1
+while i >= 0:
+    print(months[i])
+    i -= 1
+
+nums = (1,2,3,(4,5),6,7)
+print(nums)
+
+print(nums[0])
+
+print(nums[3])
+
+print(nums[3][1])
+
---
+++ learningday12/tuples.py
@@ -0,0 +1,28 @@
+#x = (1,2,3)
+#3 in x
+
+#x[0] = "change me!"
+
+#alphabet = ('a', 'b', 'c', 'd')
+#print(alphabet)
+
+#alphabet.append('e')
+
+months = ('January', "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December")
+
+print(months)
+
+print(months[0])
+
+print(months[11])
+
+locations = {
+    (35.6895, 39.6917): "Tokyo Office",
+    (40.7128, 74.0060): "New York Office",
+    (37.7749, 122.4194): "San Francisco Office"
+}
+
+print(locations[(37.7749, 122.4194)])
+
+locs = {[35,39]: 'Tokyo Office'}
+
